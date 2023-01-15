79c97b368ec3d94af717bf8af9edbc31948579c9
林忠炟 <98337638+CQJTU-Linzd@users.noreply.github.com>
2023-01-15 13:09:58 +0800

    Create 34-GLeftAndRRight.cpp

---
+++ 34-38/34-GLeftAndRRight.cpp
@@ -0,0 +1,20 @@
+// 让G放左，B放右的最少交换次数
+class MinSwap {
+public:
+    int minSwap(string s) {
+        int L = 0;
+        int index = 0;
+        int ans = 0;
+        while (s[L] == 'G') {
+            L++;
+        }
+        index = L + 1;
+        while (index < s.size()) {
+            if (s[index] == 'B') continue;
+            else if (s[index] == 'G') {
+                ans += index++ - L++;
+            }
+        }
+        return ans;
+    }
+};
